c5f977c110911469deca4e637699bceb1ddcf986
drt9739 <87109163+drt9739@users.noreply.github.com>
2022-02-26 23:34:05 +0900

    fix 2

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ spotify_redirect_uri = 'https://google.com/'
 
 # ID of your playlist for requests
 spotify_playlist_id = 'playlist_url'
+username_spotify = 'spotify username'
 
 # twitch token
 TOKEN_TWITCH = 'Token'
